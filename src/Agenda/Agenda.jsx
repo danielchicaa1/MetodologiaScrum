a4db8b45eb3ab7f7484f04fb1bf64d2e8180851a
danielchicaa1 <desarrollo@academicos.cesde.edu.co>
2023-04-14 11:38:51 -0500

    cambios

--- src/Agenda/Agenda.jsx
+++ src/Agenda/Agenda.jsx
@@ -12,17 +12,23 @@ export function Agenda() {
                 </div>
 
                 <div className="row">
-                    <div className="col-12">
-                        <div class="input-group mb-3">
-                            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
-                                <span class="input-group-text" id="basic-addon2">@example.com</span>
+                    <div className="col-6">
+                    <div class="input-group mb-3">
+                            <span class="input-group-text" id="basic-addon1">@</span>
+                            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
                         </div>
                     </div>
                 </div>
 
+                
 
                 <div className="row">
-
+                    <div className="col-6">
+                    <div class="input-group mb-3">
+                            <span class="input-group-text" id="basic-addon1">@</span>
+                            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+                        </div>
+                    </div>
                 </div>
             </form>
         </>
